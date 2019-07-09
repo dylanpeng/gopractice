3750812b8f942426390a9e6d81fed0ec7f4ae1b0
peng.chen <peng.chen@opay-inc.com>
2019-07-09 17:01:46 +0800

    2019-07-09 log lib zap, sort log file lib lumberjack

--- src/go.mod
+++ src/go.mod
@@ -3,12 +3,13 @@ module gopractice
 go 1.12
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/smartystreets/goconvey v0.0.0-20190330032615-68dc04aab96a
 	github.com/stretchr/testify v1.3.0
-	github.com/uber-go/zap v1.10.0 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0
-	golang.org/x/tools v0.0.0-20190328211700-ab21143f2384
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- src/projects/logdemo/zapdemo/logger.go
+++ src/projects/logdemo/zapdemo/logger.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
 	"time"
 )
 
 func main() {
+	LumberjackLog()
+}
+
+func QuickLoggerDemo() {
 	url := "http://www.google.com"
 
 	//print format log
@@ -46,4 +54,107 @@ func main() {
 		"backoff", time.Second,
 	)
 	sugarPro.Errorf("sugarPro Failed to fetch URL: %s", url)
+}
+
+func ConfigLogDemo() {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder, // 小写编码器
+		EncodeTime:     zapcore.ISO8601TimeEncoder,    // ISO8601 UTC 时间格式
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder, // 全路径编码器
+	}
+
+	atom := zap.NewAtomicLevelAt(zap.DebugLevel)
+
+	config := zap.Config{
+		Level:            atom,                                                // 日志级别
+		Development:      true,                                                // 开发模式，堆栈跟踪
+		Encoding:         "console",                                           // 输出格式 console json
+		EncoderConfig:    encoderConfig,                                       // 编码器配置
+		InitialFields:    map[string]interface{}{"serviceName": "spikeProxy"}, // 初始化字段，如：添加一个服务器
+		OutputPaths:      []string{"stdout", "./logs/sample.log"},             // 输出到指定文件 stdout（标准输出，正常颜色） stderr（错误输出，红色）
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	//构建日志
+	logger, err := config.Build()
+	if err != nil {
+		panic(fmt.Sprintf("log 初始化失败: %v", err))
+	}
+	logger.Info("log 初始化成功")
+
+	logger.Info("无法获取网址",
+		zap.String("url", "http://www.baidu.com"),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+
+	logger.Error("log error")
+
+}
+
+func LumberjackLog() {
+	hook := lumberjack.Logger{
+		Filename:   "./logs/lumberjacklog.log", // 日志文件路径
+		MaxSize:    1,                          // 每个日志文件保存的最大尺寸 单位：M
+		MaxBackups: 300,                        // 日志文件最多保存多少个备份
+		MaxAge:     7,                          // 文件最多保存多少天
+		Compress:   false,                      // 是否压缩
+		LocalTime:  true,                       // 是否用当地时间命名文件
+	}
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "linenum",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
+		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
+		EncodeDuration: zapcore.SecondsDurationEncoder, //
+		EncodeCaller:   zapcore.FullCallerEncoder,      // 全路径编码器
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+
+	// 设置日志级别
+	atomicLevel := zap.NewAtomicLevel()
+	atomicLevel.SetLevel(zap.InfoLevel)
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),                                        // 编码
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook)), // 打印
+		atomicLevel,                                                                     // 日志
+	)
+
+	// 开启开发模式，堆栈跟踪
+	caller := zap.AddCaller()
+	// 开启文件及行号
+	development := zap.Development()
+	// 设置初始化字段
+	filed := zap.Fields(zap.String("serviceName", "serviceName"))
+	// 构建日志
+	logger := zap.New(core, caller, development, filed)
+
+	var count int
+	for {
+		logger.Info("log 初始化成功")
+		logger.Info("无法获取网址",
+			zap.String("url", "http://www.baidu.com"),
+			zap.Int("attempt", 3),
+			zap.Duration("backoff", time.Second))
+
+		count++
+		if count > 10000 {
+			break
+		}
+	}
 }
