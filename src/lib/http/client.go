d76dfa564123f12ba6527b9de7c26a017ad77c54
peng.chen <peng.chen@opay-inc.com>
2019-07-19 11:10:57 +0800

    2019-07-19 http lib

---
+++ src/lib/http/client.go
@@ -0,0 +1,57 @@
+package http
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+type Client struct {
+	client  *http.Client
+	Url     string
+	ReqType string
+	Header  map[string]string
+	Body    []byte
+}
+
+func (c *Client) Request() ([]byte, error) {
+	req, err := http.NewRequest(c.ReqType, c.Url, bytes.NewReader(c.Body))
+
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range c.Header {
+		req.Header.Add(k, v)
+	}
+
+	resp, err := c.client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return respBody, nil
+}
+
+func NewClient(url string, header map[string]string, reqType string, body []byte) *Client {
+	client := &Client{
+		client:  &http.Client{},
+		Url:     url,
+		ReqType: reqType,
+		Header:  header,
+		Body:    body,
+	}
+
+	return client
+}
---
+++ src/lib/http/request.go
@@ -0,0 +1,77 @@
+package http
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+func Get(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return respBody, nil
+}
+
+func Post(url string, body []byte) ([]byte, error) {
+	resp, err := http.Post(url, "application/json;charset=utf-8", bytes.NewReader(body))
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+	return respBody, nil
+}
+
+func Request(url string, header map[string]string, reqType string, body []byte) ([]byte, error) {
+	req, err := http.NewRequest(reqType, url, bytes.NewReader(body))
+
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range header {
+		req.Header.Add(k, v)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return respBody, nil
+}
