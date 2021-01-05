da4c15afdd32e86f5e0b4bb0cf2f78555182848d
peng.chen <peng.chen@opay-inc.com>
2021-01-05 11:13:39 +0800

    20200105 update

--- src/projects/main.go
+++ src/projects/main.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
-	"gopractice/projects/testdemo/goconveydemo"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	if goconveydemo.IsEqual(1, 1) {
-		fmt.Printf("true")
-	} else {
-		fmt.Printf("false")
+	for i:= 0; i < 100; i++ {
+		probabilityMap := map[int64]int{1: 20, 2: 80}
+		winId := WinningLottery(probabilityMap)
+		fmt.Printf("winid: %d\n", winId)
 	}
+}
+
+
+func WinningLottery(probabilityMap map[int64]int) (winId int64) {
+	rand.Seed(time.Now().UnixNano())
+	var total, n int
+
+	for _, v := range probabilityMap {
+		total += v
+	}
+
+	rNum := rand.Intn(total)
+
+	for k, v := range probabilityMap {
+		if rNum >= n && rNum < n+v {
+			return k
+		}
+		n += v
+	}
+
+	return
 }
